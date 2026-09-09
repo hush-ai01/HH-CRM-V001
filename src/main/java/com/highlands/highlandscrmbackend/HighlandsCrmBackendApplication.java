bc54e670fbe715c123fce00abb6ac826ba668a18
Mzwandile Lucky Dladla <mzwandiledladla7@gmail.com>
2026-09-09 19:36:21 +0200

    chore: initialize Highlands CRM backend

---
+++ src/main/java/com/highlands/highlandscrmbackend/HighlandsCrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.highlands.highlandscrmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighlandsCrmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighlandsCrmBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/highlands/highlandscrmbackend/HighlandsCrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highlands.highlandscrmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighlandsCrmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
